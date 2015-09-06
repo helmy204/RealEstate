0b091b8d00c7613008494f55564f7771551d3f07
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-09-06 10:06:48 +0200

    Adding Solution

---
+++ RealEstate/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using RealEstate.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public MongoDatabase Database;
+
+        public HomeController()
+        {
+            var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+            var server = client.GetServer();
+            Database = server.GetDatabase(Settings.Default.RealEstateDatabaseName);
+        }
+
+        public ActionResult Index()
+        {
+            Database.GetStats();
+            return Json(Database.Server.BuildInfo, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
