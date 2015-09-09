15d24d37c15545e30fcdc5a7119a0ffaee5bf77a
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-09-09 09:39:37 +0200

    CH 03 Finished

--- Tests/PocoTests.cs
+++ Tests/PocoTests.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization;
 
 namespace Tests
 {
@@ -17,19 +19,64 @@ namespace Tests
             JsonWriterSettings.Defaults.Indent = true;
         }
 
+        [BsonIgnoreExtraElements]
         public class Person
         {
+            [BsonId]
+            public int PersonId { get; set; }
+
             public string FirstName { get; set; }
             public int Age { get; set; }
 
             public List<string> Address = new List<string>();
+            
             public Contact Contact = new Contact();
+            
+            [BsonIgnore]
+            public string IgnoreMe { get; set; }
+            [BsonElement("New")]
+            public string Old { get; set; }
+            private string Encapsulated;
+            [BsonElement]
+            private string Encapsulated2;
+            [BsonIgnoreIfNull]
+            public string BsonIgnoreIfNullElement { get; set; }
+            
+            [BsonRepresentation(BsonType.Double)]
+            public decimal NetWorth { get; set; }
+
+            public DateTime BirthTime { get; set; }
+            [BsonDateTimeOptions(Kind=DateTimeKind.Local)]
+            public DateTime BirthTimeLocal { get; set; }
+
+            public DateTime BirthDate { get; set; }
+            [BsonDateTimeOptions(DateOnly=true)]
+            public DateTime BirthDateOnly { get; set; }
         }
 
         public class Contact
         {
             public string Email { get; set; }
             public string Phone { get; set; }
+        }
+
+        [TestMethod]
+        public void SerializationAttributes()
+        {
+            var person = new Person();
+            person.NetWorth = 100.5m;
+            person.BirthTime = new DateTime(2014, 1, 2, 11, 30, 0);
+            person.BirthTimeLocal = new DateTime(2014, 1, 2, 11, 30, 0);
+
+            person.BirthDate = new DateTime(2014, 1, 2);
+            person.BirthDateOnly = new DateTime(2014, 1, 2);
+
+            var personJson = person.ToJson();
+            Console.WriteLine(personJson);
+
+            var personObject = BsonSerializer.Deserialize<Person>(personJson);
+            Console.WriteLine(personObject.BirthTime);
+            Console.WriteLine(personObject.BirthTimeLocal);
         }
 
         [TestMethod]
---
+++ Tests/Rentals/RentalTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RealEstate.Rentals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace Tests.Rentals
+{
+    [TestClass]
+    public class RentalTests
+    {
+        // Trible A Syntax
+        // A : Action you are testing
+        // A : Arrangement or the scenario of the test
+        // A : Assertion
+        [TestMethod]
+        public void ToDocument_RentalWithPrice_PriceRepresentedAsDouble()
+        {
+            // Arrange
+            var rental = new Rental();
+            rental.Price = 1;
+
+            // Act
+            var document = rental.ToBsonDocument();
+            
+            // Assert
+            Assert.AreEqual(document["Price"].BsonType, BsonType.Double);
+        }
+
+        [TestMethod]
+        public void ToDocument_RentalWithAnId_IdIsRepresentedAsAnObjectId()
+        {
+            // Arrange
+            var rental = new Rental();
+            rental.Id = ObjectId.GenerateNewId().ToString();
+
+            // Act
+            var document = rental.ToBsonDocument();
+
+            // Assert
+            Assert.AreEqual(document["_id"].BsonType, BsonType.ObjectId);
+
+        }
+    }
+}
