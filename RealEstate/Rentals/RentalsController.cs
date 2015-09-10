976224b9a147e5ec3be1460ce28ca5557b2248da
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-09-10 12:06:54 +0200

    Untracked Files

---
+++ RealEstate/Rentals/RentalsController.cs
@@ -0,0 +1,35 @@
+﻿using RealEstate.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.Rentals
+{
+    public class RentalsController : Controller
+    {
+        public readonly RealEstateContext Context = new RealEstateContext();
+
+        public ActionResult Index()
+        {
+            var rentals = Context.Rentals.FindAll();
+            return View(rentals);
+        }
+
+        //
+        // GET: /Rentals/Post
+        public ActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(PostRental postRental)
+        {
+            var rental = new Rental(postRental);
+            Context.Rentals.Insert(rental);
+            return RedirectToAction("Index");
+        }
+	}
+}
