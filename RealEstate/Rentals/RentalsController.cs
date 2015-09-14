3007d5b619ad02b8743a44f2d250d58d20c4dfb9
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-09-14 08:54:38 +0200

    CH05 L07

--- RealEstate/Rentals/RentalsController.cs
+++ RealEstate/Rentals/RentalsController.cs
@@ -1,10 +1,13 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
 using RealEstate.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MongoDB.Driver.Linq;
 
 namespace RealEstate.Rentals
 {
@@ -12,11 +15,65 @@ namespace RealEstate.Rentals
     {
         public readonly RealEstateContext Context = new RealEstateContext();
 
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    var rentals = Context.Rentals.FindAll();
+        //    return View(rentals);
+        //}
+
+        //public ActionResult Index(RentalsFilter filters)
+        //{
+        //    var rentals = FilterRentals(filters)
+        //        .SetSortOrder(SortBy<Rental>.Ascending(r => r.Price));
+        //    var model = new RentalsList
+        //    {
+        //        Rentals = rentals,
+        //        Filters = filters
+        //    };
+        //    return View(model);
+        //}
+
+        public ActionResult Index(RentalsFilter filters)
         {
-            var rentals = Context.Rentals.FindAll();
-            return View(rentals);
+            var rentals = FilterRentals(filters);
+            var model = new RentalsList
+            {
+                Rentals = rentals,
+                Filters = filters
+            };
+            return View(model);
         }
+
+        private IEnumerable<Rental> FilterRentals(RentalsFilter filters)
+        {
+            IQueryable<Rental> rentals = Context.Rentals.AsQueryable()
+                .OrderBy(r => r.Price);
+
+            if (filters.MinimumRooms.HasValue)
+            {
+                rentals = rentals.Where(r => r.NumberOfRooms >= filters.MinimumRooms);
+            }
+
+            if (filters.PriceLimit.HasValue)
+            {
+                var query = Query<Rental>.LTE(r => r.Price, filters.PriceLimit);
+                rentals = rentals.Where(r => query.Inject());
+            }
+
+            return rentals;
+        }
+
+
+        //private MongoCursor<Rental> FilterRentals(RentalsFilter filters)
+        //{
+        //    if (!filters.PriceLimit.HasValue)
+        //    {
+        //        return Context.Rentals.FindAll();
+        //    }
+
+        //    var query = Query<Rental>.LTE(r => r.Price, filters.PriceLimit);
+        //    return Context.Rentals.Find(query);
+        //}
 
         //
         // GET: /Rentals/Post
@@ -53,5 +110,11 @@ namespace RealEstate.Rentals
             Context.Rentals.Save(rental);
             return RedirectToAction("Index");
         }
-	}
+
+        public ActionResult Delete(string id)
+        {
+            Context.Rentals.Remove(Query.EQ("_id", new ObjectId(id)));
+            return RedirectToAction("Index");
+        }
+    }
 }
